ea4d639e96a79baaac69f0a71e3748fa58440357
Sigi Jaeckel <sigij55@gmail.com>
2020-06-12 12:46:12 -0400

    running into issue with character object

---
+++ starwars/src/API.js
@@ -0,0 +1,2 @@
+export const BASE_URL = 'https://swapi.dev/api/people/';
+// export const API_KEY = 1;
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,14 @@
 // Write your Character component here
+import React, {useState} from 'react'
+
+
+const Character = props => {
+    const { listOfCharacters } = props
+    // const { characterInfo } = props
+
+    return (
+        <div>{listOfCharacters[0].name}</div>
+    )
+}
+
+export default Character;
